07f1a7984236b8b392894a8c875f7778bbcb7dc0
taha uygun <tahauygun@gmail.com>
2019-07-30 23:38:08 -0700

    adding push linked list

--- Singly_Linked_List.js
+++ Singly_Linked_List.js
@@ -5,9 +5,21 @@ class Node{
   }
 }
 
-let first = new Node("Hi");
-first.next = new Node("there");
-first.next.next = new Node("how");
-first.next.next.next = new Node("are");
-first.next.next.next.next = new Node("you");
+class SinglyLinkedList{
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0
+  }
+  push(val){
+
+  }
+}
+
+
+// let first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
 
