d1ddb41f11483c600d7afc268e15d5756810f23f
taha uygun <tahauygun@gmail.com>
2019-07-30 23:58:24 -0700

    adding unshift

--- Singly_Linked_List.js
+++ Singly_Linked_List.js
@@ -50,6 +50,17 @@ class SinglyLinkedList {
     this.length--;
     return currentHead;
   }
+
+  unshift(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+  }
 }
 
 let list = new SinglyLinkedList();
