c0cd16861ecb1ef52df0cc921ff0ff13a604fdfa
taha uygun <tahauygun@gmail.com>
2019-07-30 23:46:42 -0700

    adding traverse

--- Singly_Linked_List.js
+++ Singly_Linked_List.js
@@ -23,6 +23,13 @@ class SinglyLinkedList{
     this.length++;
     return this
   }
+  traverse(){
+    let current = this.head;
+    while(current){
+      console.log(current.val);
+      current = current.next;
+    }
+  }
 }
 
 let list = new SinglyLinkedList()
