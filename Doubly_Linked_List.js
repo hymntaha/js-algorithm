eba7a0fd6de543abde4f004084d54ce818ee0f00
taha uygun <tahauygun@gmail.com>
2019-08-09 21:45:13 -0700

    adding unshift

--- Doubly_Linked_List.js
+++ Doubly_Linked_List.js
@@ -55,6 +55,20 @@ class DoublyLinkedList{
     return oldHead;
   }
 
+  unshift(val){
+    let newNode = new Node(val);
+    if(this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
   get(index) {
     if(index < 0 || index >= this.length) return null;
     let count,current;
