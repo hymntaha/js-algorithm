925896647a7a749b5569554fd734672d6d8277df
taha uygun <tahauygun@gmail.com>
2019-08-19 23:39:43 -0700

    adding hashtable class

--- Hash.js
+++ Hash.js
@@ -18,3 +18,21 @@ function hash(key,arrayLen){
     }
     return total;
 }
+
+//HashTable
+class HashTable{
+    constructor(size=53){
+	this.keyMap = new Array(size);
+    }
+
+    _hash(key){
+	let total = 0;
+        let WEIRD_PRIME = 31;
+        for(let i = 0; i < Math.min(key.length, 100); i++){
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * WEIRD_PRIME + value) % arrayLen;
+         }  
+         return total;
+      }
+}
