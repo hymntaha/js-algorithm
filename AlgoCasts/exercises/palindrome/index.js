6fd281358ef81dfe5085aef791cf7b6f17859fe3
taha uygun <tahauygun@gmail.com>
2019-08-20 23:31:50 -0700

    palindrome solutin 2

--- AlgoCasts/exercises/palindrome/index.js
+++ AlgoCasts/exercises/palindrome/index.js
@@ -8,10 +8,9 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  if( str === str.split('').reverse().join('')){
-    return true;
-  }
-  return false;
+  return str.split('').every((char, i)=>{
+    return char === str[str.length - i - 1];
+  })
 }
 
 module.exports = palindrome;
