0fabb6d75b49eab169feca3c4da5020f0ddc224b
taha uygun <tahauygun@gmail.com>
2019-08-22 16:17:07 -0700

    second solution for capitalize

--- AlgoCasts/exercises/capitalize/index.js
+++ AlgoCasts/exercises/capitalize/index.js
@@ -6,6 +6,22 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+function capitalize(str){
+   let result = str[0].toUpperCase();
+
+   for( let i = i; i < str.length; i++){
+       if( str[i-1] === ''){
+           result +=str[i].toUpperCase();
+       }else {
+           result += str[i];
+       }
+   }
+
+   return result;
+
+}
+
+
 
 function capitalize(str) {
     const words = [];
