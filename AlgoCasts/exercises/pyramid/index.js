bd81715e1660d7b6a3f22ce32bd9cec94011019a
taha uygun <tahauygun@gmail.com>
2019-08-22 20:20:07 -0700

    recursive version

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -14,23 +14,26 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-    const midpoint = Math.floow((2*n-1)/2);
+function pyramid(n, row = 0, level='') {
+   if(row === n){
+       return;
+   }
 
-    for(let row=0; row<n; row++){
-        let level = '';
+   if(level.length === 2 * n -1){
+       console.log(level);
+       return pyramid(n, row+1)
+   }
 
-        for(let column = 0; column<2 * n -1; column){
-           if(midpoint - row <= column && midpoint + row >=column){
-               level +='#';
-            
-           } else {
-               level += ' ';
-           }
-        }
+   const midpoint = Math.floor((2*n-1)/2);
+   let add;
+   if(midpoint- row<=level.length && midpoint + row >= level.length){
+       add = '#';
 
-        console.log(level)
-    }
+   } else {
+       add = ' ';
+   }
+
+   pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
