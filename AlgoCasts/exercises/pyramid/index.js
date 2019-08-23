1b3db7f9c381197097b553b8c9591a233a6695cf
taha uygun <tahauygun@gmail.com>
2019-08-22 19:59:27 -0700

    pyramid solution 1

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const midpoint = Math.floow((2*n-1)/2);
+
+    for(let row=0; row<n; row++){
+        let level = '';
+
+        for(let column = 0; column<2 * n -1; column){
+           if(midpoint - row <= column && midpoint + row >=column){
+               level +='#';
+            
+           } else {
+               level += ' ';
+           }
+        }
+    }
+}
 
 module.exports = pyramid;
