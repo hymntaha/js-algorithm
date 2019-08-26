e89a33b769139b9e160ad538250809997ad50b39
taha uygun <tahauygun@gmail.com>
2019-08-25 23:32:47 -0700

    starting linked list class

--- AlgoCasts/exercises/linkedlist/index.js
+++ AlgoCasts/exercises/linkedlist/index.js
@@ -3,14 +3,16 @@
 // See 'directions' document
 
 class Node {
-    constructor(data, next){
+    constructor(data, next = null){
         this.data = data;
         this.next = next;
     }
 }
 
 class LinkedList {
-
+    constructor(){
+        this.head = null;
+    }
 }
 
 module.exports = { Node, LinkedList };
