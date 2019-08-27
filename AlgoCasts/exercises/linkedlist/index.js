94aaa5b7e510cf39ff1477171b13a803193cbea7
taha uygun <tahauygun@gmail.com>
2019-08-26 21:12:32 -0700

    insertAt() method

--- AlgoCasts/exercises/linkedlist/index.js
+++ AlgoCasts/exercises/linkedlist/index.js
@@ -120,9 +120,25 @@ class LinkedList {
         if(!previous || !previous.next){
             return;
         }
-        
+
         previous.next = previous.next.next;
 
+    }
+
+    insertAt(data,index){
+        if(!this.head){
+            this.head = new Node(data);
+            return;
+        }
+
+        if(index === 0){
+            this.head = new Node(data,this.head);
+            return;
+        }
+
+        const previous = this.getAt(index-1);
+        const node = new Node(data, previous.next);
+        previous.next = node;
     }
 }
 
