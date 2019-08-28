792595ac120155a5a5923f45ef79758ed1ef4dd3
taha uygun <tahauygun@gmail.com>
2019-08-27 22:56:30 -0700

    remove method

--- AlgoCasts/exercises/tree/index.js
+++ AlgoCasts/exercises/tree/index.js
@@ -19,6 +19,11 @@ class Node {
     add(data){
         this.children.push(new Node(data));
     }
+    remove(data){
+        this.children.filter(node=>{
+            return node.data !== data;
+        });
+    }
 }
 
 class Tree {}
