8b4223298c4dcf84e52c81e8bdb90b068d807b2c
taha uygun <tahauygun@gmail.com>
2019-08-27 23:40:28 -0700

    traverse func

--- AlgoCasts/exercises/tree/index.js
+++ AlgoCasts/exercises/tree/index.js
@@ -32,7 +32,12 @@ class Tree {
     }
 
     traverseBF(fn){
-        
+        const arr = [this.root];
+        while(arr.length){
+            const node = arr.shift();
+
+            arr.push(...node.children)
+        }
     }
 }
 
