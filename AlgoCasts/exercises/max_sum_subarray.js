8b02fe404cfda9fed905e3819f83de81287e7d7c
taha uygun <tahauygun@gmail.com>
2019-08-29 20:36:15 -0700

    more elegant solution

--- AlgoCasts/exercises/max_sum_subarray.js
+++ AlgoCasts/exercises/max_sum_subarray.js
@@ -1,20 +1,19 @@
 const max_sub_array_of_size_k = function(k, arr) {    const result = [];
     let windowStart = 0;
     let windowSum = 0;
+    let maxSum = 0;
 
     for(windowEnd = 0; windowEnd<arr.length; windowEnd++){
         windowSum += arr[windowEnd];
-        console.log(windowSum)
 
         if(windowEnd >= K-1){
-            result.push(windowSum);
+            maxSum = Math.max(maxSum, windowSum);
             windowSum -= arr[windowStart];
             windowStart += 1;
         }
     }
 
-    const largest = result.sort()
-    return largest[largest.length -1 ];
+    return maxSum;
 }
 
 max_sum_subarray(3,[2, 1, 5, 1, 3, 2])
