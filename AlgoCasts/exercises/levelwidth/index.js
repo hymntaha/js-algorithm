827c0dda48b3df5b29716a12ba192bd3046454ea
taha uygun <tahauygun@gmail.com>
2019-08-31 21:39:16 -0700

    level width solution

--- AlgoCasts/exercises/levelwidth/index.js
+++ AlgoCasts/exercises/levelwidth/index.js
@@ -22,8 +22,13 @@ function levelWidth(root) {
         if(node === 's'){
             counters.push(0);
             arr.push('s');
+        } else {
+            arr.push(...node.children);
+            counters[counters.length-1]++;
         }
     }
+
+    return counters;
 }
 
 module.exports = levelWidth;
