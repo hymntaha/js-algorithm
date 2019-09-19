b8950f07728ee418ada8c51c6591138803d24815
taha uygun <tahauygun@gmail.com>
2019-09-19 02:58:11 -0700

    search triplets solution 2

--- AlgoCasts/exercises/search_triplets.js
+++ AlgoCasts/exercises/search_triplets.js
@@ -1,37 +1,39 @@
-function search_triplets(arr) {
-  arr.sort((a, b) => a - b);
-  const triplets = [];
-  for (i = 0; i < arr.length; i++) {
-    if (i > 0 && arr[i] === arr[i - 1]) { // skip same element to avoid duplicate triplets
+var threeSum = function(nums) {
+  var rtn = [];
+  if (nums.length < 3) {
+    return rtn;
+  }
+  nums = nums.sort(function(a, b) {
+    return a - b;
+  });
+
+  // (-3,-2, -1, 0, 1, 1, 2)
+  for (var i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) {
+      return rtn;
+    }
+    if (i > 0 && nums[i] == nums[i - 1]) {
       continue;
     }
-    search_pair(arr, -arr[i], i + 1, triplets);
-  }
-
-  return triplets;
-}
-
-
-function search_pair(arr, target_sum, left, triplets) {
-  let right = arr.length - 1;
-  while (left < right) {
-    const current_sum = arr[left] + arr[right];
-    if (current_sum === target_sum) { // found the triplet
-      triplets.push([-target_sum, arr[left], arr[right]]);
-      left += 1;
-      right -= 1;
-      while (left < right && arr[left] === arr[left - 1]) {
-        left += 1; // skip same element to avoid duplicate triplets
+    for (var j = i + 1, k = nums.length - 1; j < k;) {
+      if (nums[i] + nums[j] + nums[k] === 0) {
+        rtn.push([nums[i], nums[j], nums[k]]);
+        j++;
+        k--;
+        while (j < k && nums[j] == nums[j - 1]) {
+          j++;
+        }
+        while (j < k && nums[k] == nums[k + 1]) {
+          k--;
+        }
+      } else if (nums[i] + nums[j] + nums[k] > 0) {
+        k--;
+      } else {
+        j++;
       }
-      while (left < right && arr[right] === arr[right + 1]) {
-        right -= 1; // skip same element to avoid duplicate triplets
-      }
-    } else if (target_sum > current_sum) {
-      left += 1; // we need a pair with a bigger sum
-    } else {
-      right -= 1; // we need a pair with a smaller sum
     }
   }
-}
+  return rtn;
+};
 
-console.log(`Search triplets: ${search_triplets([-5, 2, -1, -2, 3])}`);
+console.log(threeSum([-3, 0, 1, 2, -1, 1, -2]));
