ed0558da9c4324b706f048eec025e2a4883000de
taha uygun <tahauygun@gmail.com>
2019-12-06 23:05:55 -0800

    another solution

--- AlgoCasts/exercises/find_disappeared_nums.js
+++ AlgoCasts/exercises/find_disappeared_nums.js
@@ -1,5 +1,12 @@
-let findDisappearedNumbers = function(nums) {
-
-};
-
-findDisappearedNumbers([4,3,2,7,8,2,3,1])
+var findDisappearedNumbers = function (nums) {
+  let length = nums.length
+  let arr = []
+  for (let i = 0; i < length; i++) {
+    arr[i] = i+1
+  }
+  for (v of nums) {
+    arr[v-1] = -1
+  }
+  return arr.filter( i => (i > 0) )
+}
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
