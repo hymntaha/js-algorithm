bed2de5ed57d92fd7570e7e1b8fa0108547d946d
taha uygun <tahauygun@gmail.com>
2019-12-27 02:40:13 -0800

    solution for dutch flag

---
+++ AlgoCasts/exercises/move_zeros.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let temp;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+      temp = num[num.length-1]
+    }
+  }
+};
+
+moveZeroes([0,1,0,3,12])
