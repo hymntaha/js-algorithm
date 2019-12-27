649fd374172a1e37f4f158ae062077ec15a9df6b
taha uygun <tahauygun@gmail.com>
2019-12-27 14:20:06 -0800

    some leetcode solutions

---
+++ AlgoCasts/exercises/jewels_and_stones.js
@@ -0,0 +1,6 @@
+var numJewelsInStones = function(J, S) {
+  const jewels = new Set(J)
+  return S.split('').reduce((res, s) => res + jewels.has(s), 0)
+};
+
+console.log(numJewelsInStones('aA','aAAbbbb'))
---
+++ AlgoCasts/exercises/quadruple_sum_to_target.js
@@ -0,0 +1,7 @@
+const search_quadruplets = function(arr, target) {
+  quadruplets = [];
+  // TODO: Write your code here
+  return quadruplets;
+};
+
+console.log(search_quadruplets([4, 1, 2, -1, 1, -3],1));
