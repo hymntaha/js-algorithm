ea45f059b5e84dc266c995d8c77ad12418cbecbc
taha uygun <tahauygun@gmail.com>
2019-12-29 00:57:11 -0800

    solution for jewel and stone

--- AlgoCasts/exercises/jewels_and_stones.js
+++ AlgoCasts/exercises/jewels_and_stones.js
@@ -1,6 +1,15 @@
-var numJewelsInStones = function(J, S) {
-  const jewels = new Set(J)
-  return S.split('').reduce((res, s) => res + jewels.has(s), 0)
-};
+// var numJewelsInStones = function(J, S) {
+//   const jewels = new Set(J)
+//   console.log(jewels);
+//   S.split('').reduce((res, s) => {
+//     console.log(res)
+//     res + jewels.has(s), 0)
+//   }
+//   return
+// };
+
+const numJewelsInStones = (J, S) => S.split('').filter(char => J.indexOf(char) !== -1).length;
 
 console.log(numJewelsInStones('aA','aAAbbbb'))
+
+//another good solution
