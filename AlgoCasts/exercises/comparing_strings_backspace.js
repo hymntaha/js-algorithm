6ece37876eb4db00394ca919576df2d45f0ef103
taha uygun <tahauygun@gmail.com>
2019-12-30 23:31:38 -0800

    correct solution

--- AlgoCasts/exercises/comparing_strings_backspace.js
+++ AlgoCasts/exercises/comparing_strings_backspace.js
@@ -1,24 +1,47 @@
-const backspace_compare = function(str1, str2) {
-  let arr1= str1.split(''), arr2=str2.split('');
-  // let str1Pound = str1.indexOf('#');
-  // let str2Pound = str2.indexOf('#');
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i]==='#' ){
-      arr1.splice(i-1,i)
+function backspace_compare(str1, str2) {
+  // use two pointer approach to compare the strings
+  let index1 = str1.length - 1,
+    index2 = str2.length - 1;
+  while (index1 >= 0 || index2 >= 0) {
+    let i1 = get_next_valid_char_index(str1, index1),
+      i2 = get_next_valid_char_index(str2, index2);
+    if (i1 < 0 && i2 < 0) { // reached the end of both the strings
+      return true;
     }
-  }
-
-  for (let i = 0; i < arr2.length; i++) {
-    if (arr2[i]==='#' ) {
-      arr2.splice(i - 1, i)
+    if (i1 < 0 || i2 < 0) { // reached the end of one of the strings
+      return false;
     }
-  }
+    if (str1[i1] !== str2[i2]) { // check if the characters are equal
+      return false;
+    }
 
-  if(arr1.toString() == arr2.toString()){
+    index1 = i1 - 1;
+    index2 = i2 - 1;
+
     return true;
   }
-  return false;
-};
+}
+
+
+function get_next_valid_char_index(str, index) {
+  let backspaceCount = 0;
+  while (index >= 0) {
+    if (str[index] === '#') { // found a backspace character
+      backspaceCount += 1;
+    } else if (backspaceCount > 0) { // a non-backspace character
+      backspaceCount -= 1;
+    } else {
+      break;
+    }
+
+    index -= 1; // skip a backspace or a valid character
+  }
+
+  return index;
+}
+
 
 console.log(backspace_compare('xy#z', 'xzz#'));
+console.log(backspace_compare('xy#z', 'xyz#'));
+console.log(backspace_compare('xp#', 'xyz##'));
+console.log(backspace_compare('xywrrmp', 'xywrrmu#p'));
