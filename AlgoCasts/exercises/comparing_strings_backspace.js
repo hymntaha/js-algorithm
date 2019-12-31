11f0ffad8ae7bf5152acf5b34be7b4f6dc1c6bbd
taha uygun <tahauygun@gmail.com>
2019-12-30 23:25:46 -0800

    solution for comparing strings

--- AlgoCasts/exercises/comparing_strings_backspace.js
+++ AlgoCasts/exercises/comparing_strings_backspace.js
@@ -1,8 +1,24 @@
 const backspace_compare = function(str1, str2) {
-  // TODO: Write your code here
+  let arr1= str1.split(''), arr2=str2.split('');
+  // let str1Pound = str1.indexOf('#');
+  // let str2Pound = str2.indexOf('#');
 
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i]==='#' ){
+      arr1.splice(i-1,i)
+    }
+  }
 
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i]==='#' ) {
+      arr2.splice(i - 1, i)
+    }
+  }
+
+  if(arr1.toString() == arr2.toString()){
+    return true;
+  }
   return false;
 };
 
-backspace_compare('xy#z', 'xzz#');
+console.log(backspace_compare('xy#z', 'xzz#'));
